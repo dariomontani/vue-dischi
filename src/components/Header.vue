0fb0c328a0919675277c81c749d665c8b545d08d
dariomontani <dariomontani@gmail.com>
2022-01-18 16:32:53 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .container{
+        background-color: #2E3A46;
+        img{
+            width: 5em;
+            padding: 1em;
+        }
+    }
+</style>
