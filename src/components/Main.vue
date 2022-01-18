74528c95e675b52757457d14cb1e7f182ad373e9
dariomontani <dariomontani@gmail.com>
2022-01-18 16:11:13 +0100

    aggiunta di bootstrap e del main

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data(){
+        return{
+            queryPath: "https://flynn.boolean.careers/exercises/api/array/music",
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "~bootstrap/scss/bootstrap";
+</style>
